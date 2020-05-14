307f2c3a5993a48b15ceff98b0cd1b66eb43e542
unknown <eqnope@gmail.com>
2020-05-14 23:46:22 +0100

    Updates

---
+++ precache-manifest.86afbc8743cbdf7d30e68ff10384b8a7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f7628ff94f7e4e6a1883d3e8318ddd",
+    "url": "/mtsf/index.html"
+  },
+  {
+    "revision": "7b06598613f3c4dec032",
+    "url": "/mtsf/static/css/main.c7c421d8.chunk.css"
+  },
+  {
+    "revision": "0d433d812c8ae3e0fd33",
+    "url": "/mtsf/static/js/2.7b118212.chunk.js"
+  },
+  {
+    "revision": "02c3a19524f06317e2b0adad925a4b32",
+    "url": "/mtsf/static/js/2.7b118212.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b06598613f3c4dec032",
+    "url": "/mtsf/static/js/main.6ef339a3.chunk.js"
+  },
+  {
+    "revision": "2c3c6d9731fcd5708814",
+    "url": "/mtsf/static/js/runtime-main.50d5ac53.js"
+  },
+  {
+    "revision": "ce4f14f581d94aa3e00dd40c7bb88c6f",
+    "url": "/mtsf/static/media/client_side.ce4f14f5.png"
+  },
+  {
+    "revision": "c89a87fd8c3be5a72c8d3f4e32a96b4b",
+    "url": "/mtsf/static/media/laptop_again.c89a87fd.svg"
+  },
+  {
+    "revision": "8140a700fc0a0823924b99d920ff4519",
+    "url": "/mtsf/static/media/led.8140a700.jpeg"
+  },
+  {
+    "revision": "fb002781920f022d6dc55a1bd10b5456",
+    "url": "/mtsf/static/media/linux.fb002781.png"
+  },
+  {
+    "revision": "0539a7f52b594b13e2b126230f8ed0ed",
+    "url": "/mtsf/static/media/moreCrypto.0539a7f5.png"
+  },
+  {
+    "revision": "41268bd438f5a29e38ec1fe4d3bd853a",
+    "url": "/mtsf/static/media/video.41268bd4.mp4"
+  }
+]);
