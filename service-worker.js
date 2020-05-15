add78c0616cbc567939d8b55a0d02e90d78e429e
unknown <eqnope@gmail.com>
2020-05-15 20:08:33 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0eea57d6fed2184e325df079f6449873.js"
+  "/precache-manifest.e3e29ea438487f113879df1a2616e0d1.js"
 );
 
 self.addEventListener('message', (event) => {
